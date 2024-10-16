cbce1d55773ee5fdc2f0c5507a5e075dfa47dbc2
Allan <6740989+allan2@users.noreply.github.com>
2024-10-16 11:37:13 -0400

    clippy cast_lossless

--- src/unix.rs
+++ src/unix.rs
@@ -287,7 +287,7 @@ impl MmapInner {
         let populate = if populate { MAP_POPULATE } else { 0 };
         let hugetlb = if huge.is_some() { MAP_HUGETLB } else { 0 };
         let offset = huge
-            .map(|mask| ((mask as u64) & (MAP_HUGE_MASK as u64)) << MAP_HUGE_SHIFT)
+            .map(|mask| (u64::from(mask) & (MAP_HUGE_MASK as u64)) << MAP_HUGE_SHIFT)
             .unwrap_or(0);
         MmapInner::new(
             len,
